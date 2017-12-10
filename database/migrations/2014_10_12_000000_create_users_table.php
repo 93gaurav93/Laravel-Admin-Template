294fc3f36c9202742fddf564969fdaa4e091a312
Gaurav Deshpande <93gaurav93@gmail.com>
2017-12-10 23:12:28 +0530

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 128)->unique();
             $table->string('password');
             $table->integer('level')->default(0);
             $table->integer('user_id')->default(1);
